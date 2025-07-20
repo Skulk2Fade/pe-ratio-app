ee52699616978bc2c8d3a8bf51388379d9b0b344
Skulk2Fade <120564980+Skulk2Fade@users.noreply.github.com>
2025-07-21 09:31:51 +1000

    Add comments on public watchlists and portfolios

--- stockapp/models.py
+++ stockapp/models.py
@@ -118,3 +118,23 @@ class PortfolioFollow(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     follower_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     followed_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+
+class WatchlistComment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    watchlist_owner_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    user = db.relationship("User", foreign_keys=[user_id])
+    owner = db.relationship("User", foreign_keys=[watchlist_owner_id])
+
+
+class PortfolioComment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    portfolio_owner_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    user = db.relationship("User", foreign_keys=[user_id])
+    owner = db.relationship("User", foreign_keys=[portfolio_owner_id])
